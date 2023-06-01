45b31f50a73efcec8da789ffaa054a7edb7b96f9
PhyoWunnaLin <phyowunnalin1214@gmail.com>
2023-06-01 21:25:40 +0630

    unRouteGuard

--- src/routes/Path.jsx
+++ src/routes/Path.jsx
@@ -3,13 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Dashboard from "../pages/Dashboard";
-import RouteGuard from "../components/RouteGuard";
 
 const Path = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<RouteGuard><Dashboard/></RouteGuard>} />
+        <Route path="/" element={<Dashboard/>} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
