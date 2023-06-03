0ccbbc45037c4edbf70770c84fd6965fe88c951b
PhyoWunnaLin <phyowunnalin1214@gmail.com>
2023-06-03 10:26:46 +0630

    user css

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -14,31 +14,33 @@ import { BsArrowLeft } from "react-icons/bs";
 import { Link, useParams } from "react-router-dom";
 import Cookies from "js-cookie";
 import { useGetUserInfoQuery } from "../redux/api/contactApi";
+import '../css/user.css';
 
 const UserInfo = () => {
   const { id } = useParams();
   const token = Cookies.get("token");
   const { data, isLoading } = useGetUserInfoQuery({ id, token });
   const user = data?.contact;
-  console.log(user);
 
-  if(isLoading){
-    return(
-        <div className="flex justify-center items-center h-screen">
-            <h1 className=" bg-sky-600 text-white text-lg font-bold rounded px-5 py-1">Loading....</h1>
-        </div>
-    )
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <h1 className=" bg-sky-600 text-white text-lg font-bold rounded px-5 py-1">
+          Loading....
+        </h1>
+      </div>
+    );
   }
   return (
     <>
-      <div className=" flex justify-center my-16 items-center">
-        <div className="w-[80%] shadow-lg p-5 flex justify-center rounded relative py-10">
+      <div className=" flex justify-center my-16 max-sm:my-0 items-center">
+        <div className="w-[80%] max-sm:w-[100%] sm:shadow-lg sm:p-5 max-sm:py-5 flex justify-center rounded relative py-10">
           <Link to={"/"}>
             <div className=" absolute left-2 top-2 bg-gray-50 rounded-full p-2 hover:bg-gray-100 hover:shadow duration-500">
               <BsArrowLeft className=" text-xl cursor-pointer" />
             </div>
           </Link>
-          <div className="w-[80%] flex flex-col gap-10">
+          <div className="w-[80%] lg:w-[95%] flex flex-col gap-10">
             <div className=" p-1 flex max-md:flex-col justify-between items-center gap-8">
               <div className="flex max-md:flex-col items-center gap-4">
                 <div className=" p-12 bg-cyan-700 w-40 max-sm:w-32 max-sm:p-8 flex justify-center items-center rounded-full">
@@ -47,7 +49,9 @@ const UserInfo = () => {
                   </p>
                 </div>
                 <div className="flex flex-col gap-2">
-                  <p className=" text-2xl font-medium max-md:mx-auto tracking-wide">{user?.name}</p>
+                  <p className=" text-2xl font-medium max-md:mx-auto tracking-wide">
+                    {user?.name}
+                  </p>
                   <p className=" border rounded-xl cursor-pointer max-md:mx-auto hover:shadow-md hover:bg-gray-50 duration-500 border-gray-400 flex items-center px-3 gap-3 justify-center py-1 font-medium">
                     <BsSendPlusFill className=" text-teal-700" /> Label
                   </p>
@@ -64,7 +68,7 @@ const UserInfo = () => {
               </div>
             </div>
             <div className="flex flex-col justify-center items-center">
-              <hr className=" my-3 border border-gray-300 mx-auto w-[100%] relative" />
+              <hr className=" my-3 border border-gray-300 mx-auto w-[100%]" />
               <div className=" flex gap-5 absolute z-100">
                 <p className="border bg-gray-50 hover:shadow hover:bg-gray-200 text-xl cursor-pointer duration-500 border-gray-300 rounded-full p-2">
                   <HiOutlineMail className=" text-teal-600 max-sm:text-sm" />
@@ -81,7 +85,7 @@ const UserInfo = () => {
               </div>
             </div>
             <div className=" flex max-lg:flex-col max-lg:gap-7 gap-10">
-              <div className="flex flex-col gap-3 border border-gray-300 rounded w-[100%] lg:w-[70%] xl:w-[60%] p-3">
+              <div className="flex flex-col gap-3 border border-gray-300 rounded w-[100%] xl:w-[60%] p-3">
                 <h2 className="font-medium text-lg">Contact Detail</h2>
                 <div className="flex gap-3 items-center">
                   <MdOutlineMarkEmailRead className=" text-lg text-gray-500" />
@@ -93,23 +97,23 @@ const UserInfo = () => {
                 </div>
                 <div className="flex gap-3 items-center">
                   <LuCake className=" text-lg text-gray-500" />
-                  <p className=" tracking-wide">null</p>
+                  <p className=" tracking-wide">1/1/2001</p>
                 </div>
                 <div className="flex gap-3 items-center">
                   <FaRegAddressCard className=" text-lg text-gray-500" />
                   <p className=" tracking-wide">{user?.address}</p>
                 </div>
               </div>
-              <div className="xl:w-[40%] lg:w-[80%] w-[100%] flex flex-col gap-3">
+              <div className="xl:w-[40%] w-[100%] flex flex-col gap-3">
                 <div className=" flex items-center gap-4">
                   <p className=" text-lg font-medium">History</p>
                   <TbInfoOctagon className=" text-gray-500 text-lg" />
                 </div>
-                <div className=" flex max-sm:flex-col gap-1">
+                <div className=" flex responsive gap-1">
                   <p>Last edited :</p>
                   <p className=" text-gray-600">Yesterday, 9:43 PM</p>
                 </div>
-                <div className=" flex max-sm:flex-col gap-1">
+                <div className=" flex responsive gap-1">
                   <p>Added to contacts :</p>
                   <p className=" text-gray-600">Yesterday, 9:43 PM</p>
                 </div>
