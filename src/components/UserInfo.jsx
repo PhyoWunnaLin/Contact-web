c05a73782080e5c7e117cd8cd0ff51509f89ee6e
PhyoWunnaLin <phyowunnalin1214@gmail.com>
2023-06-03 14:00:16 +0630

    info bg

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -24,7 +24,7 @@ const UserInfo = () => {
 
   if (isLoading) {
     return (
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center h-screen">
         <h1 className=" bg-sky-600 text-white text-lg font-bold rounded px-5 py-1">
           Loading....
         </h1>
@@ -33,7 +33,7 @@ const UserInfo = () => {
   }
   return (
     <>
-      <div className=" flex justify-center py-16 max-sm:py-0 items-center sm:h-screen bg-blue-200 ">
+      <div className=" flex justify-center py-16 max-sm:py-0 sm:bg-blue-200 max-sm:bg-blue-100 ">
         <div className="w-[80%] max-sm:w-[100%] sm:shadow-lg sm:p-5 max-sm:py-5 flex justify-center rounded bg-blue-100 relative py-10">
           <Link to={"/"}>
             <div className=" absolute left-2 top-2 bg-blue-200 font-bold rounded-full p-2  hover:bg-gray-100 hover:shadow duration-500">
